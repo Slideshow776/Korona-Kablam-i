b73334e6e451cc2c0e4145f8bade4ed74e170608
Sandra Moen <sandramoen01@gmail.com>
2020-05-03 13:02:19 +0200

    animated title

---
+++ core/src/no/sandramoen/koronakablami/actors/TitleActor.kt
@@ -0,0 +1,45 @@
+package no.sandramoen.koronakablami.actors
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.glutils.ShaderProgram
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.scenes.scene2d.Stage
+import no.sandramoen.koronakablami.utils.BaseActor
+
+class TitleActor(x: Float, y: Float, s: Stage) : BaseActor(x, y, s) {
+    var isAnimated = false
+
+    private var vertexShaderCode: String
+    private var fragmenterShaderCode: String
+    private var shaderProgram: ShaderProgram
+    var time = .0f
+
+    init {
+        loadImage("title3")
+        width = Gdx.graphics.width * .1f
+        height = Gdx.graphics.height * .20f * (Gdx.graphics.width.toFloat() / Gdx.graphics.height.toFloat())
+
+        vertexShaderCode = Gdx.files.internal("shaders/default.vs").readString()
+        fragmenterShaderCode = Gdx.files.internal("shaders/wave.fs").readString()
+        shaderProgram = ShaderProgram(vertexShaderCode, fragmenterShaderCode)
+        if (!shaderProgram.isCompiled)
+            Gdx.app.error("WaveBackground.kt", "Shader compile error: " + shaderProgram.log)
+    }
+
+    override fun draw(batch: Batch, parentAlpha: Float) {
+        if (isAnimated) batch.shader = shaderProgram
+        shaderProgram.setUniformf("u_time", time)
+        shaderProgram.setUniformf("u_imageSize", Vector2(width, height))
+        shaderProgram.setUniformf("u_amplitude", Vector2(.2f, .01f))
+        shaderProgram.setUniformf("u_wavelength", Vector2(10f, 1f))
+        shaderProgram.setUniformf("u_velocity", Vector2(7f, 1f))
+        super.draw(batch, parentAlpha)
+        if (isAnimated) batch.shader = null
+    }
+
+    override fun act(dt: Float) {
+        super.act(dt)
+        time += dt
+    }
+}
--- core/src/no/sandramoen/koronakablami/screens/gameplay/LevelScreen.kt
+++ core/src/no/sandramoen/koronakablami/screens/gameplay/LevelScreen.kt
@@ -3,10 +3,13 @@ package no.sandramoen.koronakablami.screens.gameplay
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.Interpolation
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Stack
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
 import no.sandramoen.koronakablami.actors.*
 import no.sandramoen.koronakablami.utils.BaseActor
 import no.sandramoen.koronakablami.utils.BaseGame
@@ -24,6 +27,7 @@ class LevelScreen : BaseScreen() {
     private var playerMayShoot = false
     private var gameOver = true
     private var pause = true
+    private var titleAnimationPlaying = true
     private var score = 0L
 
     private var enemyTimer = 0f
@@ -32,14 +36,17 @@ class LevelScreen : BaseScreen() {
 
     private lateinit var scoreLabel: Label
     private lateinit var overlayScoreLabel: Label
-    private lateinit var title: BaseActor
+    private lateinit var title0: BaseActor
+    private lateinit var title1: BaseActor
+    private lateinit var title2: BaseActor
+    private lateinit var title3: TitleActor
+    private lateinit var title4: BaseActor
     private lateinit var highScoreLabel: Label
     private lateinit var touchToStartLabel: Label
 
     override fun initialize() {
         width = Gdx.graphics.width.toFloat()
         height = Gdx.graphics.height.toFloat()
-        // ratio = width / height
 
         BaseGame.levelMusic!!.isLooping = true
         BaseGame.levelMusic!!.volume = BaseGame.audioVolume
@@ -79,10 +86,18 @@ class LevelScreen : BaseScreen() {
         scoreLabel.setFontScale(.5f)
         scoreLabel.isVisible = false
 
-        title = BaseActor(0f, 0f, uiStage)
-        title.loadImage("title")
-        title.width = width * .98f
-        title.height = Gdx.graphics.height * .20f * (width / height)
+        title0 = createTitleFragment("title0", .39f)
+        title1 = createTitleFragment("title1", .14f)
+        val titleStack = Stack()
+        title2 = createTitleFragment("title2", .34f)
+        title3 = TitleActor(width / 2 - width * .49f, height / 2, mainStage)
+        title4 = createTitleFragment("title4", .1f)
+        titleStack.add(title3)
+        titleStack.add(title4)
+
+        titleStack.toBack()
+        title2.toFront()
+        title2.zIndex = 99
 
         highScoreLabel = Label("High Score: ${BaseGame.highScore}", BaseGame.labelStyle)
         highScoreLabel.setFontScale(.55f)
@@ -102,19 +117,24 @@ class LevelScreen : BaseScreen() {
         val uiTable = Table()
         uiTable.setFillParent(true)
 
-        uiTable.add(scoreLabel).expandY().top().padTop(height * .01f).row()
-        uiTable.add(title).row()
-        uiTable.add(highScoreLabel).row()
-        uiTable.add(overlayScoreLabel).row()
-        uiTable.add(touchToStartLabel).padBottom(height * .15f).row()
+        uiTable.add(scoreLabel).expandY().top().padTop(height * .01f).colspan(4).row()
+        uiTable.add(title0)
+        uiTable.add(title1)
+        uiTable.add(title2)
+        uiTable.add(titleStack).row()
+        uiTable.add(highScoreLabel).colspan(4).row()
+        uiTable.add(overlayScoreLabel).colspan(4).row()
+        uiTable.add(touchToStartLabel).colspan(4).padTop(height * .01f).padBottom(height * .15f).row()
         uiTable.add().expandY()
 
         /*uiTable.debug = true*/
         uiStage.addActor(uiTable)
+
+        startTitleAnimation()
     }
 
     override fun update(dt: Float) {
-        if (pause)
+        if (titleAnimationPlaying)
             return
 
         enemyTimer += dt
@@ -134,7 +154,7 @@ class LevelScreen : BaseScreen() {
                 enemySpeed = 400f
         }
 
-        if (gameOver)
+        if (gameOver || pause)
             return
 
         playerMayShoot = BaseActor.count(mainStage, Laser::class.java.canonicalName) <= 2
@@ -166,7 +186,7 @@ class LevelScreen : BaseScreen() {
     override fun keyDown(keycode: Int): Boolean { // desktop controls
         if (keycode == Keys.SPACE && playerMayShoot)
             player.shoot()
-        else if (keycode == Keys.ENTER && gameOver) {
+        else if (keycode == Keys.ENTER && gameOver && !titleAnimationPlaying) {
             gameOver = false
             pause = false
             playerMayShoot = true
@@ -181,7 +201,7 @@ class LevelScreen : BaseScreen() {
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
         if (playerMayShoot)
             player.shoot()
-        if (gameOver) {
+        if (gameOver && !titleAnimationPlaying) {
             gameOver = false
             pause = false
             playerMayShoot = true
@@ -192,16 +212,22 @@ class LevelScreen : BaseScreen() {
 
     private fun renderOverlay() {
         scoreLabel.isVisible = !scoreLabel.isVisible
-        title.isVisible = !title.isVisible
+        title0.isVisible = !title0.isVisible
+        title1.isVisible = !title1.isVisible
+        title2.isVisible = !title2.isVisible
+        title3.isVisible = !title3.isVisible
+        title4.isVisible = !title4.isVisible
         highScoreLabel.isVisible = !scoreLabel.isVisible
         overlayScoreLabel.isVisible = !scoreLabel.isVisible
         touchToStartLabel.isVisible = !scoreLabel.isVisible
-        if (gameOver) {
+        if (gameOver) { // viewing the menu
+            startTitleAnimation()
             overlayScoreLabel.setText("Score: $score")
-        } else {
+        } else { // playing
             score = 0
             scoreLabel.setText("Score: $score")
             player.isVisible = true
+            player.setPosition(Gdx.graphics.width / 2f - player.width / 2f, Gdx.graphics.height * .03f)
 
             for (enemy: BaseActor in BaseActor.getList(mainStage, Enemy::class.java.canonicalName))
                 enemy.remove()
@@ -233,5 +259,57 @@ class LevelScreen : BaseScreen() {
             highScoreLabel.setText("New High Score: ${BaseGame.highScore}")
             highScoreLabel.color = Color.PURPLE
         }
+    }
+
+    private fun createTitleFragment(name: String, widthPercent: Float): BaseActor {
+        val actor = BaseActor(0f, 0f, uiStage)
+        actor.loadImage(name)
+        actor.width = width * widthPercent
+        actor.height = Gdx.graphics.height * .20f * (width / height)
+        return actor
+    }
+
+    private fun startTitleAnimation() {
+        // setup
+        touchToStartLabel.isVisible = false
+        titleAnimationPlaying = true
+        title3.isAnimated = false
+
+        // animation
+        val animationSpeed = 1.0f // use this to change the total animation speed (bigger is slower, lesser is faster)
+        title1.addAction(Actions.sequence( // "KA"
+                Actions.delay(.25f * animationSpeed),
+                Actions.parallel(
+                        Actions.sequence(
+                                /*Actions.moveBy(0f, title1.height * -.4f),*/
+                                Actions.moveBy(0f, title1.height * -.07f, 1.1f * animationSpeed, Interpolation.exp10In)
+                        ),
+                        Actions.sizeTo(title1.width, title1.height * 1.5f, 1.1f * animationSpeed, Interpolation.exp10In)
+                ),
+                Actions.delay(.125f),
+                Actions.parallel(
+                        Actions.sizeTo(title1.width, title1.height * .5f, .1f * animationSpeed),
+                        Actions.moveBy(0f, title1.height * .13f, .1f * animationSpeed)
+                ),
+                Actions.delay(.5f),
+                Actions.parallel(
+                        Actions.sizeTo(title1.width, title1.height, .5f * animationSpeed),
+                        Actions.moveBy(0f, title1.height * -.06f, .5f * animationSpeed)
+                )
+        ))
+        title2.addAction(Actions.sequence( // "BLAM"
+                Actions.delay(1.575f * animationSpeed),
+                Actions.sizeTo(title2.width * 1.2f, title2.width * 1.2f, .2f * animationSpeed, Interpolation.swingOut),
+                Actions.run { // "i!"
+                    title3.isAnimated = true
+                    title3.addAction(Actions.fadeIn(2f * animationSpeed))
+                    title4.addAction(Actions.fadeIn(2f * animationSpeed))
+                },
+                Actions.sizeTo(title2.width, title2.height, .5f * animationSpeed, Interpolation.bounceOut),
+                Actions.run {
+                    titleAnimationPlaying = false
+                    touchToStartLabel.isVisible = true
+                }
+        ))
     }
 }
