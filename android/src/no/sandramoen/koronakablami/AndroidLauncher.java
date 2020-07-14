03db831e39c5e1f8f9d0b7cc84bb83204b08c020
Sandra Moen <sandramoen01@gmail.com>
2020-07-14 13:58:03 +0200

    release

--- android/src/no/sandramoen/koronakablami/AndroidLauncher.java
+++ android/src/no/sandramoen/koronakablami/AndroidLauncher.java
@@ -1,20 +1,19 @@
 package no.sandramoen.koronakablami;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import no.sandramoen.koronakablami.KoronaKablamIGame;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.useAccelerometer = true;
-		config.useCompass = false;
-		config.useImmersiveMode = true;
-		config.useWakelock = true;
-		initialize(new KoronaKablamIGame(), config);
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useAccelerometer = true;
+        config.useCompass = false;
+        config.useImmersiveMode = true;
+        config.useWakelock = true;
+        initialize(new KoronaKablamIGame(), config);
+    }
 }
